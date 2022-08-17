f9135da5df156c33f6474f7c57d8ecc845406dd7
Jared Love <lovejared91@yahoo.com>
2022-08-17 16:37:48 -0500

    added format for Date, time, and current day

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,20 @@ var two = $("#14pm");
 var three = $("#15pm");
 var four = $("#16pm");
 var five = $("#17pm");
+
+
+//Format Date and time 
+var date = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
+var hour = moment().format('h:mm:ss a');
+var hours = moment().hours();
+var input;
+var hourt;
+
+//Format for current day
+var interval = setInterval(function() {
+  var time = moment();
+  $('#currentDay').html(time.format('YYYY MMMM DD') + ' '
+                      + time.format('dddd')
+                       .substring(0,3).toUpperCase());
+  $('#currentDay').html(date + " " + time.format('hh:mm:ss A'));
+}, 100);
