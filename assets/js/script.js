19536936c07cab340875a2398f7070a06dbc66a0
Jared Love <lovejared91@yahoo.com>
2022-08-17 16:36:51 -0500

    added js file and variables

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//varaibles used
+var nine = $("#9am");
+var ten = $("#10am");
+var eleven = $("#11am");
+var twelve = $("#12pm");
+var one = $("#13pm");
+var two = $("#14pm");
+var three = $("#15pm");
+var four = $("#16pm");
+var five = $("#17pm");
